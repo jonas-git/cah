dd2399ee61c4e822933bd204a697b51ad730eaaf
jonas-git <jonas.vanen@gmail.com>
2018-05-10 20:39:33 +0200

    Fixed indentation and changed logging

--- socket.js
+++ socket.js
@@ -1,14 +1,13 @@
 const createSocket = require('socket.io');
-
-
+const debug = require('debug')('cah:server');
 
 module.exports = function (server) {
-    const io = createSocket(server);
+  const io = createSocket(server);
 
-    io.on('connection', function (socket) {
-        console.log('a user established a connection');
-        socket.on('login', function (credentials) {
-            console.log('the user logged in with the following credentials:', credentials);
-        });
+  io.on('connection', function (socket) {
+    debug('A user established a connection');
+    socket.on('login', function (credentials) {
+      debug('The user logged in with the following credentials:', credentials);
     });
+  });
 };
