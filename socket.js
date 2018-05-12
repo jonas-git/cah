7367e39cd4ce294132c67b58e6c3afee8bd53682
jonas-git <jonas.vanen@gmail.com>
2018-05-13 01:35:47 +0200

    Added client and client list classes

--- socket.js
+++ socket.js
@@ -19,7 +19,7 @@ module.exports = function (server) {
     // Once a connection is established,
     // immediately send the client configuration.
     socket.emit('config', clientConfig);
-    
+
     socket.on('login', function (credentials) {
       console.log('The user logged in with the following credentials:', credentials);
     });
@@ -39,4 +39,54 @@ function createUUID(checkUUID) {
     uuidB62 = base62.encode(uuid);
   } while (typeof checkUUID === 'function' && !checkUUID(uuidB62));
   return uuidB62;
+}
+
+/**
+ * A client that connected to the socket.
+ * @param {string} name Name of the client.
+ * @param {object} socket Socket.io connection object.
+ */
+function Client(name, socket) {
+  this.uuid = createUUID(x => !(x in state.users));
+  this.name = name;
+  this.socket = socket;
+}
+
+/**
+ * Change the name of the client.
+ * @param {string} newName New name of the client.
+ */
+Client.prototype.rename = function (newName) {
+  this.name = newName;
+};
+
+/**
+ * Data structure to keep track of all connected clients.
+ */
+function ClientList() {
+  this.list = {};
+}
+
+/**
+ * Add a client to the list.
+ * @param {Client} client Any client.
+ */
+ClientList.prototype.push = function (client) {
+  this.list[client.uuid] = client;
+};
+
+/**
+ * Remove a client from the list.
+ * @param {Client} client A client that is present in the list.
+ */
+ClientList.prototype.pop = function (client) {
+  delete this.list[client.uuid];
+}
+
+/**
+ * Find a client in the list by its UUID.
+ * @param {string} uuid UUID of a client.
+ */
+ClientList.prototype.find = function (uuid) {
+  return uuid in this.list ? this.list[uuid] : undefined;
 }
