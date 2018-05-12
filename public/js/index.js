358bfd6b6d87890c48fe350108e7b13f143e6cb9
jonas-git <jonas.vanen@gmail.com>
2018-05-12 18:29:20 +0200

    Sending config on connection and using it

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,30 @@
 const login_form = document.querySelector('.login-container form');
 const login_input_field = login_form.querySelector('input#user-name');
 const login_submit_button = login_form.querySelector('button');
+const loading_container = document.querySelector('.loading-container');
 
-// The user name input field must contain at least one character:
-const user_name_min_length = 1;
-login_input_field.addEventListener('input', function (e) {   
-  login_submit_button.disabled = this.value.length < user_name_min_length;
-});
+const socket = io();
+let config = null;
 
-const socket = io(); // initialise a socket connection
+// The login submit button is disabled by default,
+// since the input field does not contain anything yet.
+login_submit_button.disabled = true;
 
-login_submit_button.addEventListener('click', function (e) {
-  // send the login credentials
-  socket.emit('login', {
-    'user-name': login_input_field.value
+socket.on('config', function (data) {
+  config = data;
+
+  // The user name input field has a minimum amount of characters.
+  login_input_field.addEventListener('input', function (e) {   
+    login_submit_button.disabled = this.value.length < config.min_name_length;
   });
+
+  login_submit_button.addEventListener('click', function (e) {
+    // Send the login credentials.
+    socket.emit('login', {
+      'user-name': login_input_field.value
+    });
+  });
+
+  // Disable the loading screen.
+  loading_container.classList.add('disabled');
 });
--- socket.js
+++ socket.js
@@ -1,9 +1,13 @@
 const createSocket = require('socket.io');
+const configValue = require('./config');
+const gameConfig = configValue('Game');
 
 module.exports = function (server) {
   const io = createSocket(server);
 
   io.on('connection', function (socket) {
+    socket.emit('config', gameConfig);
+    
     console.log('A user established a connection');
 
     socket.on('login', function (credentials) {
