f2659d175084ec0c1f008820bd440a6c5879c576
jonas-git <jonas.vanen@gmail.com>
2018-05-12 18:43:08 +0200

    Using game object

--- socket.js
+++ socket.js
@@ -1,14 +1,15 @@
 const createSocket = require('socket.io');
 const configValue = require('./config');
-const gameConfig = configValue('Game');
+
+const game = {
+  config: configValue('Game')
+};
 
 module.exports = function (server) {
   const io = createSocket(server);
 
   io.on('connection', function (socket) {
-    socket.emit('config', gameConfig);
-    
-    console.log('A user established a connection');
+    socket.emit('config', game.config);
 
     socket.on('login', function (credentials) {
       console.log('The user logged in with the following credentials:', credentials);
