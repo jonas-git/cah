1d3a1476e45e78c13987bac4c8b63ca7fe2938c1
jonas-git <jonas.vanen@gmail.com>
2018-05-13 05:14:31 +0200

    Moved event listener definition outside socket event handler For each acknowledged log in another event listener would be created which is redundant

--- public/js/index.js
+++ public/js/index.js
@@ -35,6 +35,40 @@ socket.on('config', function (config) {
 });
 
 const intro_name_span = main_container.querySelector('span#intro-name');
+
+socket.on('login_ack', function (client) {
+  _client = client;
+  intro_name_span.innerText = _client.name;
+
+  // The login has been acknowledged, so show the main screen.
+  main_container.classList.remove('disabled');
+  login_container.classList.add('disabled');
+  loading_container.classList.add('disabled');
+});
+
+(function () {
+  let oldName = null;
+
+  // Once the users starts renaming store the old name,
+  // in case the new name would be too short.
+  intro_name_span.addEventListener('focus', function (e) {
+    oldName = this.innerText;
+  });
+
+  // When enter is pressed save the new name.
+  intro_name_span.addEventListener('keypress', function (e) {
+    if (e.which === 13) {
+      e.preventDefault();
+      const name = this.innerText;
+      if (name.length >= _config.min_name_length)
+        socket.emit('rename', name);
+      else
+        this.innerText = oldName;
+      this.blur();
+    }
+  });
+})();
+
 const main_toolbar = main_container.querySelector('menu.main-toolbar');
 const lobby_toolset = main_toolbar.querySelector('.lobby-toolset');
 const game_toolset = main_toolbar.querySelector('.game-toolset');
@@ -42,54 +76,21 @@ const global_toolset = main_toolbar.querySelector('.global-toolset');
 const create_game_button = lobby_toolset.querySelector('button.create-game');
 const logout_button = global_toolset.querySelector('button.logout');
 
-socket.on('login_ack', function (client) {
-  _client = client;
-  intro_name_span.innerText = _client.name;
+logout_button.addEventListener('click', function (e) {
+  // Reset everything to the initial state.
+  login_submit_button.disabled = true;
+  login_input_field.value = '';
+  loading_container.classList.remove('disabled');
+  login_container.classList.remove('disabled');
+  main_container.classList.add('disabled');
+  lobby_container.classList.remove('disabled');
+  game_container.classList.add('disabled');
+  lobby_toolset.classList.remove('disabled');
+  game_toolset.classList.add('disabled');
 
-  (function () {
-    let oldName = null;
-
-    // Once the users starts renaming store the old name,
-    // in case the new name would be too short.
-    intro_name_span.addEventListener('focus', function (e) {
-      oldName = this.innerText;
-    });
-
-    // When enter is pressed save the new name.
-    intro_name_span.addEventListener('keypress', function (e) {
-      if (e.which === 13) {
-        e.preventDefault();
-        const name = this.innerText;
-        if (name.length >= _config.min_name_length)
-          socket.emit('rename', name);
-        else
-          this.innerText = oldName;
-        this.blur();
-      }
-    });
-  })();
-
-  logout_button.addEventListener('click', function (e) {
-    // Reset everything to the initial state.
-    login_submit_button.disabled = true;
-    login_input_field.value = '';
-    loading_container.classList.remove('disabled');
-    login_container.classList.remove('disabled');
-    main_container.classList.add('disabled');
-    lobby_container.classList.remove('disabled');
-    game_container.classList.add('disabled');
-    lobby_toolset.classList.remove('disabled');
-    game_toolset.classList.add('disabled');
-
-    socket.on('logout_ack', function () {
-      _client = null;
-      loading_container.classList.add('disabled');
-    });
-    socket.emit('logout');
+  socket.on('logout_ack', function () {
+    _client = null;
+    loading_container.classList.add('disabled');
   });
-
-  // The login has been acknowledged, so show the main screen.
-  main_container.classList.remove('disabled');
-  login_container.classList.add('disabled');
-  loading_container.classList.add('disabled');
+  socket.emit('logout');
 });
